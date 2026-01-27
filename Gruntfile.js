c57bb33acbc13616168442c2abd8cf34b96682ce
peterlau123 <freelaupeter@gmail.com>
2026-01-27 13:52:12 +0800

    fix: fix shirpy css error

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,8 @@ module.exports = function(grunt) {
                     paths: ["css"]
                 },
                 files: {
-                    "css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less"
+                    "css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less",
+                    "css/chirpy-sidebar.css": "less/chirpy-sidebar.less"
                 }
             },
             minified: {
@@ -24,7 +25,8 @@ module.exports = function(grunt) {
                     cleancss: true
                 },
                 files: {
-                    "css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less"
+                    "css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less",
+                    "css/chirpy-sidebar.min.css": "less/chirpy-sidebar.less"
                 }
             }
         },
